1e3b0750fbf3a4a2b3593a6f08405c98af3a71a7
王颜颜 <wangyanyanlucky@163.com>
2019-08-05 17:45:12 +0800

    first commit

---
+++ src/TestCommonComponent.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Button } from 'antd';
+export default () => (
+  <Button type="primary">公共组件</Button>
+);
---
+++ src/index.jsx
@@ -0,0 +1,3 @@
+import TestCommonComponent from './TestCommonComponent';
+
+export {TestCommonComponent}
